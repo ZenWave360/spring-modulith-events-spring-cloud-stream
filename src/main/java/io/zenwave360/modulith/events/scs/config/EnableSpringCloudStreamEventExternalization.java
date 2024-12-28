beaee45c33670fc11e35ad67d866734de8a4e245
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2024-12-28 11:49:03 +0100

    fixes Retention policy for @EnableSpringCloudStreamEventExternalization

--- src/main/java/io/zenwave360/modulith/events/scs/config/EnableSpringCloudStreamEventExternalization.java
+++ src/main/java/io/zenwave360/modulith/events/scs/config/EnableSpringCloudStreamEventExternalization.java
@@ -3,7 +3,14 @@ package io.zenwave360.modulith.events.scs.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @Configuration
 @Import({ SpringCloudStreamEventExternalizerConfiguration.class, EventSerializerConfiguration.class})
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EnableSpringCloudStreamEventExternalization {
 }
