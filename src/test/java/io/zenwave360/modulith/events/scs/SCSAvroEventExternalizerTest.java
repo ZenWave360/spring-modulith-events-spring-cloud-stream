3c44d99da7b93289ef62b9d644dbb4a6cf18a46a
Ivan Garcia Sainz-Aja <ivangsa@gmail.com>
2024-12-29 10:26:15 +0100

    completes Integration Tests

--- src/test/java/io/zenwave360/modulith/events/scs/SCSAvroEventExternalizerTest.java
+++ src/test/java/io/zenwave360/modulith/events/scs/SCSAvroEventExternalizerTest.java
@@ -2,11 +2,20 @@ package io.zenwave360.modulith.events.scs;
 
 import io.zenwave360.modulith.events.scs.config.EventSerializerConfiguration;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = { TestsConfiguration.class })
 @Import({ EventSerializerConfiguration.class })
@@ -16,17 +25,27 @@ public class SCSAvroEventExternalizerTest {
     @Autowired
     TestsConfiguration.CustomerEventsProducer customerEventsProducer;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+    @MockitoSpyBean
+    private StreamBridge streamBridge;
 
     @Test
     void testExternalizeAvroEvent() throws InterruptedException {
         var event = new io.zenwave360.modulith.events.scs.dtos.avro.CustomerEvent();
         event.setName("John Doe");
+
         customerEventsProducer.onCustomerEventAvro(event);
+
         // Wait for the event to be externalized
-        Thread.sleep(5000);
-        // TODO: Assert that the event was externalized
+        await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+            verify(streamBridge).send(Mockito.eq("customers-avro-out-0"), Mockito.argThat(message -> {
+                if (message instanceof Message<?>) {
+                    var payload = ((Message<?>) message).getPayload();
+                    return payload instanceof io.zenwave360.modulith.events.scs.dtos.avro.CustomerEvent
+                            && "John Doe".equals(((io.zenwave360.modulith.events.scs.dtos.avro.CustomerEvent) payload).getName());
+                }
+                return false;
+            }));
+        });
     }
 
 }
